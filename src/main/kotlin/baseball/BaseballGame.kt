1de1a2bc291463c0d325dacdf04bad284d21a5ae
Nahyun Kim <hedghog1004@naver.com>
2025-01-14 02:51:06 +0900

    feat: 게임 시작 문구 출력

---
+++ src/main/kotlin/baseball/BaseballGame.kt
@@ -0,0 +1,9 @@
+package baseball
+
+import baseball.constants.GAME_START_GUIDE
+
+class BaseballGame {
+    fun play() {
+        println(GAME_START_GUIDE) // 게임 시작 문구 출력
+    }
+}
---
+++ src/main/kotlin/baseball/constants/GameTerm.kt
@@ -0,0 +1,3 @@
+package baseball.constants
+
+const val GAME_START_GUIDE = "숫자 야구 게임을 시작합니다."
