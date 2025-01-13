63c43fd6c7307bf609aa87b456098e646d0fee91
Nahyun Kim <hedghog1004@naver.com>
2025-01-14 03:12:14 +0900

    feat: 난수 생성 로직 구현

--- src/main/kotlin/baseball/BaseballGame.kt
+++ src/main/kotlin/baseball/BaseballGame.kt
@@ -1,9 +1,12 @@
 package baseball
 
 import baseball.constants.GAME_START_GUIDE
+import baseball.utils.RandomNumberGenerator
 
 class BaseballGame {
     fun play() {
         println(GAME_START_GUIDE) // 게임 시작 문구 출력
+        val randomNum = RandomNumberGenerator.createRandomNumber() // 난수 생성
+//        print(randomNum)
     }
 }
---
+++ src/main/kotlin/baseball/constants/GameRule.kt
@@ -0,0 +1,7 @@
+package baseball.constants
+
+const val GAME_DIGIT = 3 // 게임을 몇 자리 숫자로 할지
+
+// 난수 생성 제한 범위
+const val RANDOM_NUMBER_RANGE_START = 1
+const val RANDOM_NUMBER_RANGE_END = 9
