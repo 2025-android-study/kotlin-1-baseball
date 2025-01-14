530e6a77ad1e2d2496072730528e0c615b3c615c
Nahyun Kim <hedghog1004@naver.com>
2025-01-14 20:53:21 +0900

    fix: 게임 결과 출력 로직 변경
    - 볼 or 스트라이크일 경우 처리

--- src/main/kotlin/baseball/views/OutputView.kt
+++ src/main/kotlin/baseball/views/OutputView.kt
@@ -6,9 +6,25 @@ import baseball.enums.MatchType
 import baseball.model.MatchCount
 
 object OutputView {
+    // 결과 출력
     fun printMatchResult(matchCount: MatchCount) {
         if (matchCount.strikeCount == 0 && matchCount.ballCount == 0) println(MatchType.NOTHING.text) // 낫싱
-        else if (matchCount.strikeCount == GAME_DIGIT) println(OUTPUT_GUESS_SUCCESS) // 게임 종료
-        else println("${matchCount.strikeCount}${MatchType.STRIKE.text} ${matchCount.ballCount}${MatchType.BALL.text}") // 힌트 출력
+        else if (matchCount.strikeCount == GAME_DIGIT) { // 숫자를 모두 맞혔을 떄
+            println(getStrikeText(GAME_DIGIT))
+            println(OUTPUT_GUESS_SUCCESS)
+        }
+        else { // 볼 or 스트라이크
+            if (matchCount.strikeCount == 0) println(getBallText(matchCount.ballCount))
+            else if (matchCount.ballCount == 0) println(getStrikeText(matchCount.strikeCount))
+            else println("${getStrikeText(matchCount.strikeCount)} ${getBallText(matchCount.ballCount)}")
+        }
+    }
+
+    private fun getStrikeText(strikeCount: Int): String {
+        return "${strikeCount}${MatchType.STRIKE.text}"
+    }
+
+    private fun getBallText(ballCount: Int): String {
+        return "${ballCount}${MatchType.BALL.text}"
     }
 }
