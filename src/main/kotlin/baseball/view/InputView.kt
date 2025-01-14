f6e3986178624a2f388380ee8f931d071f4b8a87
edv-Shin <edvedv613@gmail.com>
2025-01-14 17:35:15 +0900

    feat: view 구현

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -1,4 +1,27 @@
 package baseball.view
 
+import baseball.consts.InputConst
+import baseball.validation.InputValidator
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
+    private val validator = InputValidator()
+
+    fun getNumber(): Int {
+        print(InputConst.INPUT_NUMBER)
+        val input = Console.readLine()
+
+        // TODO 검증
+
+        return input.toInt()
+    }
+
+    fun getRestart(): Int {
+        print(InputConst.INPUT_RESTART)
+        val input = Console.readLine()
+
+        // TODO 검증
+
+        return input.toInt()
+    }
 }
---
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -0,0 +1,29 @@
+package baseball.view
+
+import baseball.consts.OutputConst
+
+class OutputView {
+    fun printResult(strike: Int, ball: Int) {
+        println(createResult(strike, ball))
+    }
+
+    fun printStart() {
+        println(OutputConst.GAME_START)
+    }
+
+    fun printFinish() {
+        println(OutputConst.GAME_FINISHED)
+    }
+
+    private fun createResult(strike: Int, ball: Int): String {
+        return when {
+            strike == 0 && ball == 0 -> OutputConst.NOTHING.toString()
+
+            ball == 0 -> String.format(OutputConst.STRIKE.toString(), strike)
+
+            strike == 0 -> String.format(OutputConst.BALL.toString(), ball)
+
+            else -> String.format(OutputConst.BALL.toString(), ball) + String.format(OutputConst.STRIKE.toString(), strike)
+        }
+    }
+}
